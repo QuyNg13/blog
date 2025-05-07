00980c89ca3bc739a4fe48a263f7128d0db59a95
Quy <quythieu34@gmail.com>
2025-05-07 22:04:25 +0200

    hackathon

--- server/server.js
+++ server/server.js
@@ -379,6 +379,28 @@ const leerdoelen = {
   },
 };
 
+const hackathon = {
+  id: 'Leerdoel 3',
+  title: 'Hackathon',
+  inleiding: 'Tijdens de hackathon moest ik samen met een team een website maken dat de "women in tech" presenteerd. Ons idee hiervoor was om een interactieve wereldkaart en klok te maken in een klaslokaal om te kunnen filteren in de afkomst en tijdperk van de vrouwen. vervolgens is het mogelijk om door de resulaten heen te scrollen een carousel en meer informatie te krijgen.',
+  content: [
+    {
+      titel: 'Mijn inbreng',
+      alinea: [
+        'Ik was verantwoordelijk voor de de klok. ik heb deze met puur CSS gemaakt en het menu dat er in zit gemaakt met behulp van enchor positioning. In principe is dit een zelf gebouwde select element. Ik heb ook grote rol gespeeld in het reparen van code van anderen en en het samenvoegen.',
+      ]
+    },
+    {
+      titel: 'Nieuwe inzichten',
+      alinea: [
+        'Ik vond het lastig om onder tijdsdruk te werken, ik ben gewend om een lange tijd onderzoek te kunnen doen naar welke code ik wil gebruiken maar dit was voor dit project erg lastig.',
+        'Ik was eerst van plan meer te gaan maken dan alleen de klok maar dit is door de tijdsdruk niet gelukt waardoor ik niet helemaal tevreden was met mijn inbreng.',
+        'Het was wel een leuke ervaring om weer eens met andere mensen iets te maken. Het is erg leuk om te zien hoe het eindproduct er uit ziet als je alles zamen hebt gevoegd.',
+      ]
+    }
+  ]
+};
+
 const engine = new Liquid({
   extname: '.liquid',
 });
@@ -405,7 +427,9 @@ app.get('/weekly-nerd', async (req, res) => {
 
 app.get('/hackaton', async (req, res) => {
   return res.send(renderTemplate('server/views/hackaton.liquid', {
-    title: 'Hackaton',
+    title: hackathon.title,
+    inleiding: hackathon.inleiding,
+    content: hackathon.content
   }));
 });
 
